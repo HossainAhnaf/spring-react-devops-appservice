cbe36cbc041c06ddeb23e85ea478f18f63571052
Your Name <you@example.com>
2025-06-28 17:24:52 +0600

    backup

--- backend/src/main/java/backend/hobbiebackend/config/CorsConfig.java
+++ backend/src/main/java/backend/hobbiebackend/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         .allowedOrigins("*")
                         .allowedMethods("*")
-                        .allowedHeaders("*")
+                        .allowedHeaders("*");
             }
         };
     }
